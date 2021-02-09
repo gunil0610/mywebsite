f55dc32238c3835d63f8435a98b737515b9461b7
Gunil <gunil.na1@gmail.com>
2021-02-09 13:41:40 +0900

    toggle btn hover pointer fix, added some translation

--- indexPlaceholder.js
+++ indexPlaceholder.js
@@ -2,7 +2,9 @@ const toggleLangBtn = document.getElementById('toggle-lang'),
   title = document.getElementById('title'),
   greetingText = document.getElementById('greeting__text'),
   previewText = document.getElementById('preview-text'),
-  aboutMeText = document.getElementById('aboutMeText');
+  aboutMeText = document.getElementById('aboutMeText'),
+  skillsText = document.getElementById('skillsText'),
+  postLink = document.getElementById('postLink');
 
 let renderedToKor = false;
 
@@ -19,6 +21,14 @@ const placeholders = {
         I'm fast learner, who love to learn more things and will grow fast with the experience.`,
       domEl: aboutMeText,
     },
+    skillsText: {
+      text: `Frontend developer aiming to be a fullstack developer in the future:`,
+      domEl: skillsText,
+    },
+    postLink: {
+      text: `Check More →`,
+      domEl: postLink,
+    },
   },
   ko: {
     title: { text: "나건일's Portfolio", domEl: title },
@@ -31,6 +41,14 @@ const placeholders = {
       text: `저는 가천대학교 전자공학과를 졸업했습니다.\n
         같이 일하고 싶은 똑똑한 개발자가 되기 위하여 배우고 노력하고 있습니다.\n`,
       domEl: aboutMeText,
+    },
+    skillsText: {
+      text: `프론트엔드 개발자로서 필요한 기술들과 약간의 백엔드 지식을 알고 있습니다. 항상 최신 기술 트렌드를 알기위해 노력합니다 :`,
+      domEl: skillsText,
+    },
+    postLink: {
+      text: `더 보기 →`,
+      domEl: postLink,
     },
   },
 };
