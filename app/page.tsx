6de3ad13e184f3a01738cb3a07d9664c78aedbd7
gunil0610 <gunil.na1@gmail.com>
2022-10-28 01:00:50 +0900

    feat: next.js 13 setup, installing tailwind

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <main className="h-screen w-full flex items-center justify-center">
+        <h1 className="text-3xl font-bold">Hello</h1>
+      </main>
+    </div>
+  );
+}
